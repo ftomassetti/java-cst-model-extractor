4f177a846986c562c5290921e3c581885e7ad5a4
Federico Tomassetti <federico@tomassetti.me>
2017-01-18 15:14:07 +0100

    first code for the analyzer

---
+++ src/main/kotlin/analyzer.kt
@@ -0,0 +1,116 @@
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ast.NodeList
+import com.github.javaparser.ast.body.MethodDeclaration
+import com.github.javaparser.ast.expr.MethodCallExpr
+import com.github.javaparser.ast.expr.StringLiteralExpr
+import com.github.javaparser.ast.expr.UnaryExpr
+import com.github.javaparser.ast.stmt.BlockStmt
+import com.github.javaparser.ast.stmt.ExpressionStmt
+import com.github.javaparser.ast.stmt.IfStmt
+import com.github.javaparser.ast.stmt.Statement
+import java.io.File
+
+val PATH_TO_JAVAPARSER_SRC = File("../javaparser/javaparser-core/src/main/java/")
+
+fun processPrintString(text: String) {
+    if (text.startsWith(" ")) {
+        println("    space")
+        processPrintString(text.substring(1))
+        return
+    }
+    if (text.endsWith(" ")) {
+        processPrintString(text.substring(0, text.length - 1))
+        println("    space")
+        return
+    }
+    println("    token '$text'")
+}
+
+fun processStatements(statements: NodeList<Statement>, index: Int = 0) {
+    if (statements.size == index) {
+        return
+    }
+    val s = statements[index]
+    if (s is ExpressionStmt && s.expression is MethodCallExpr) {
+        val mce = s.expression as MethodCallExpr
+        if (mce.name.id.equals("println")) {
+            println("    newline")
+            processStatements(statements, index + 1)
+            return
+        }
+        if (mce.name.id.equals("print")) {
+            if (mce.arguments[0] is StringLiteralExpr) {
+                processPrintString((mce.arguments[0] as StringLiteralExpr).value)
+                processStatements(statements, index + 1)
+                return
+            }
+        }
+        if (mce.name.id.equals("printJavaComment")) {
+            println("    comment")
+            processStatements(statements, index + 1)
+            return
+        }
+        // optional property
+        if (mce.name.id.equals("accept") && mce.scope.get() is MethodCallExpr
+                && (mce.scope.get() as MethodCallExpr).name.id.equals("get")
+                && (mce.scope.get() as MethodCallExpr).scope.get() is MethodCallExpr) {
+            println("    property ${((mce.scope.get() as MethodCallExpr).scope.get() as MethodCallExpr).name.id}")
+            processStatements(statements, index + 1)
+            return
+        }
+        // not optional property
+        if (mce.name.id.equals("accept") && mce.scope.get() is MethodCallExpr) {
+            println("    property ${(mce.scope.get() as MethodCallExpr).name.id}")
+            processStatements(statements, index + 1)
+            return
+        }
+    }
+    if (s is IfStmt && s.condition is MethodCallExpr && (s.condition as MethodCallExpr).name.id.equals("isPresent")) {
+        val fieldName = ((s.condition as MethodCallExpr).scope.get() as MethodCallExpr).name
+        println("    start optional on $fieldName")
+        if (s.thenStmt is BlockStmt) {
+            processStatements((s.thenStmt as BlockStmt).statements)
+        } else {
+            throw UnsupportedOperationException(s.thenStmt.javaClass.canonicalName)
+        }
+        if (s.elseStmt.isPresent) {
+            println("    else optional on $fieldName")
+            processStatements((s.elseStmt.get() as BlockStmt).statements)
+        }
+        println("    end optional on $fieldName")
+        processStatements(statements, index + 1)
+        return
+    }
+    if (s is IfStmt && s.condition is UnaryExpr &&
+            (s.condition as UnaryExpr).operator == UnaryExpr.Operator.LOGICAL_COMPLEMENT
+            && (s.condition as UnaryExpr).expression is MethodCallExpr && ((s.condition as UnaryExpr).expression as MethodCallExpr).name.id.equals("isEmpty")) {
+        val fieldName = (((s.condition as UnaryExpr).expression as MethodCallExpr).scope.get() as MethodCallExpr).name.id
+        println("    start notEmpty on $fieldName")
+        if (s.thenStmt is BlockStmt) {
+            processStatements((s.thenStmt as BlockStmt).statements)
+        } else {
+            throw UnsupportedOperationException(s.thenStmt.javaClass.canonicalName)
+        }
+        if (s.elseStmt.isPresent) {
+            println("    else notEmpty on $fieldName")
+            processStatements((s.elseStmt.get() as BlockStmt).statements)
+        }
+        println("    end notEmpty on $fieldName")
+        processStatements(statements, index + 1)
+        return
+    }
+    throw UnsupportedOperationException(s.toString())
+}
+
+fun extractConcreteSyntaxModel(visitMethod: MethodDeclaration) {
+    println("Model for ${visitMethod.parameters[0].type}")
+    processStatements(visitMethod.body.get().statements)
+    println()
+}
+
+fun main(args: Array<String>) {
+    val prettyPrintVisitorFile = File(PATH_TO_JAVAPARSER_SRC.path + File.separator + "com/github/javaparser/printer/PrettyPrintVisitor.java")
+    val prettyPrintVisitor = JavaParser.parse(prettyPrintVisitorFile)
+    prettyPrintVisitor.getClassByName("PrettyPrintVisitor").get().methods
+            .filter { it.name.id.equals("visit") }.forEach(::extractConcreteSyntaxModel)
+}
